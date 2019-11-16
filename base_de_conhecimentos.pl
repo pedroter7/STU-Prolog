b130aa9803017901d4de8c7ae887f26744f1ebce
gabriela <gabiyumi.a@gmail.com>
2019-11-16 11:51:23 -0300

    adicionado procedimento compressao

--- base_de_conhecimentos.pl
+++ base_de_conhecimentos.pl
@@ -1,4 +1,4 @@
-/* Procedimentos */
+﻿/* Procedimentos */
 
 % Limpeza
 procedimento(troca_curativo, 'Limpar ferida com SF 0,9%').
@@ -40,7 +40,20 @@ procedimento(moderada_exs, sem_infec, com_dor, 'Usar espuma de poliuretano').
 procedimento(grande_exs, sem_infec, com_dor, 'Usar espuma de poliuretano').
 
 % Compressão
-% TO DO
+procedimento(baixa_exs, sem_infec, alta_ITB, 'elevar MMII por meia hora E')
+procedimento(baixa_exs, sem_infec, moderada_ITB, 'elevar MMII por meia hora E')
+procedimento(baixa_exs, sem_infec, alta_ITB, 'usar hidrocoloide e bota de unna OU')
+procedimento(baixa_exs, sem_infec, moderada_ITB, 'usar hidrocoloide e bota de unna OU')
+procedimento(baixa_exs, sem_infec, alta_ITB, 'usar hidrocoloide e bandagem de compressão graduada elástica')
+procedimento(baixa_exs, sem_infec, moderada_ITB, 'usar hidrocoloide e bandagem de compressão graduada elástica')
+procedimento(moderada_exs, sem_infec, alta_ITB, 'elevar MMII por meia hora e usar alginatode calcio OU')
+procedimento(moderada_exs, sem_infec, moderada_ITB,'elevar MMII por meia hora e usar alginatode calcio OU')
+procedimento(moderada_exs, sem_infec, alta_ITB,'hidrofibra e bota de unna OU'
+procedimento(moderada_exs, sem_infec, moderada_ITB,'elevar MMII por meia hora eusar alginato de cálcio ou hidrofibra e bandagem de compressão graduada elástica') 
+procedimento(grande_exs, sem_infec, moderada_ITB, 'elevar MMII por meia hora e usar espuma de poliuretano OU')
+procedimento(grande_exs, sem_infec, moderada_ITB, 'espuma hidrocelular e bota unna OU')
+procedimento(grande_exs, sem_infec, moderada_ITB, 'elevar MMII por meia hora e usar espuma de poliuretano ou espuma hidrocelular e bandagem de compressao graduada')
+
 
 % Alta
 :- discontiguous procedimento/2.

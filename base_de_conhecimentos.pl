cb45d4bb42bc74d813648347397fa561713bc1c0
Pedro Tersetti Freidinger <pedrotersetti3@gmail.com>
2019-11-16 00:59:24 -0300

    Add files via upload
    Inclui ALTA e COBERTURAS

--- base_de_conhecimentos.pl
+++ base_de_conhecimentos.pl
@@ -26,6 +26,29 @@ procedimento(pele_seca, 'Usar creme restaurador de pH da pele').
 procedimento(pele_escamosa, 'Usar AGE').
 procedimento(pele_escamosa, 'Usar creme restaurador de pH da pele').
 
+% Coberturas
+procedimento(baixa_exs, sem_infec, sem_dor, 'Usar hidrocoloide').
+procedimento(moderada_exs, sem_infec, sem_dor, 'Usar alginato de calcio').
+procedimento(moderada_exs, sem_infec, sem_dor, 'Usar hidrofibra').
+procedimento(moderada_exs, com_infec, sem_dor, 'Usar alginato de calcio com prata').
+procedimento(moderada_exs, com_infec, sem_dor, 'Usar hidrofibra com prata').
+procedimento(grande_exs, sem_infec, sem_dor, 'Usar espuma de poliuretano').
+procedimento(grande_exs, sem_infec, sem_dor, 'Usar espuma hidrocelular').
+procedimento(grande_exs, com_infec, sem_dor, 'Usar espuma de poliuretano com prata').
+procedimento(baixa_exs, sem_infec, com_dor, 'Usar hidrocoloide').
+procedimento(moderada_exs, sem_infec, com_dor, 'Usar espuma de poliuretano').
+procedimento(grande_exs, sem_infec, com_dor, 'Usar espuma de poliuretano').
+
+% Compressão
+% TO DO
+
+% Alta
+:- discontiguous procedimento/2.
+procedimento(epitelizada, 'O paciente pode receber alta').
+
+/* Regras */
+
+
 /* Seção de ajuda */
 
 % Nomes dos procedimentos
@@ -34,6 +57,10 @@ nome('Ulcera com necrose -> <nome_do_procedimento>: necrose, sem <opcoes>.').
 nome('Ulcera com esfacelos -> <nome_do_procedimento>: esfacelos, sem <opcoes>.').
 nome('Pele ao redor da ulcera seca -> <nome_do_procedimento>: pele_seca, sem <opcoes>.').
 nome('Pele ao redor da ulcera escamosa -> <nome_do_procedimento>: pele_escamosa, sem <opcoes>.').
+nome('Ulcera com baixa exsudacao -> <nome_do_procedimento>: baixa_exs, <opcao>: sem_infec (sem infeccao)/com_infec (com infeccao), <opcao>: com_dor/sem_dor.').
+nome('Ulcera com moderada exsudacao -> <nome_do_procedimento>: moderada_exs, <opcao>: sem_infec (sem infeccao)/com_infec (com infeccao), <opcao>: com_dor/sem_dor.').
+nome('Ulcera com grande exsudacao -> <nome_do_procedimento>: grande_exs, <opcao>: sem_infec (sem infeccao)/com_infec (com infeccao), <opcao>: com_dor/sem_dor.').
+nome('Ulcera epitelizada -> <nome_do_procedimento>: epitelizada, sem <opcoes>.').
 
 % Regras de ajuda
 ajuda :- write('Execute: procedimento(<nome_do_procedimento>, <opcoes se for o caso>, <variavel de retorno>)'), nl, write('Execute: ajuda(procedimentos) para visualizar as possibilidades.'), nl.

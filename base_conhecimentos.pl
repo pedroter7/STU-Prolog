b7b6f700d3a8c1ff94f483d4d844c185da3cba73
Pedro Tersetti Freidinger <pedrotersetti3@gmail.com>
2019-12-07 12:29:32 -0300

    Add files via upload

--- base_conhecimentos.pl
+++ base_conhecimentos.pl
@@ -1,4 +1,23 @@
-%Interface com o usuario e runtime
+/*
+ * PERGUNTAS
+*/
+
+pergunta(limpeza) :-
+    write('E necessario troca de curativo? (y/n) '),
+    read(Resposta),
+    (Resposta==y -> procedimento(limpeza)).
+
+/*
+ * PROCEDIMENTOS
+*/
+
+procedimento(limpeza) :-
+    write('Limpe a ferida com SF 0,9%.'),
+    nl.
+
+/*
+ * RUNTIME
+*/
 
 apresentacao :-
     nl,
@@ -13,15 +32,23 @@ apresentacao :-
     write("O programa recolhera alguns dados e indicara os possiveis procedimentos a serem tomados."),
     nl.
 
+
+    coletar_dados :-
+        pergunta(limpeza).
+
+
 dados_paciente(Paciente, Idade) :-
     write("Nome do paciente (entre aspas simples): "),
     read(Paciente),
     write("Idade do paciente: "),
     read(Idade),
-    write("O sistema agora ira coletar dados sobre o quadro clinico do paciente."),
+    write("O sistema agora ira coletar dados sobre o quadro clinico do paciente "),
+    write(Paciente), write('.'),
     nl.
+
 
 comecar :-
     apresentacao,
     nl,
-    dados_paciente(Paciente, Idade).
+    dados_paciente(Paciente, Idade),
+    coletar_dados.
